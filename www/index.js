f3aa9f6446b313bd12329f6f1c66da3e7f6227f6
timchen <timchen10001@gmail.com>
2023-09-05 10:36:03 +0800

    feat: add `wat` plain code

--- www/index.js
+++ www/index.js
@@ -38,3 +38,23 @@ async function init() {
 }
 
 init();
+
+/**
+ * WebAssembly
+
+  (module
+    (import "console" "log" (func $log))
+    (import "console" "error" (func $error))
+    (memory $mem 1)
+    (data (i32.const 0) "Hi")
+    (func $sum (param $a i32) (param $b i32) (result i32)
+      call $log
+      call $error
+      local.get $a
+      local.get $b
+      i32.add
+    )
+    (export "mem" (memory $mem))
+    (export "sum" (func $sum))
+  )
+ */
